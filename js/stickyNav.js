149cdc4f5d61489052d7c499c5cfc2108e6e902a
vSEK1RO <vladlitvinov2006@gmail.com>
2023-11-28 15:09:37 +0300

    stickyNav updated to window.scrollY

--- js/stickyNav.js
+++ js/stickyNav.js
@@ -2,7 +2,7 @@ window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.scrollY >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
